38d859a1cab78a42ab99a3a58fc03483f5dd03cb
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2025-07-12 10:41:22 +0300

    Session 8.2

--- src/main/java/utils/MethodHandles.java
+++ src/main/java/utils/MethodHandles.java
@@ -35,9 +35,29 @@ public class MethodHandles {
         explicitWait(locator, time);
         webElement(locator).click();
     }
+    protected void clear(By locator, int time) {
+        explicitWait(locator, time);
+        webElement(locator).clear();
+    }
     protected void sendKeys(By locator, int time, String text) {
         explicitWait(locator, time);
         webElement(locator).sendKeys(text);
+    }
+    protected String getText(By locator, int time) {
+        explicitWait(locator, time);
+        return webElement(locator).getText();
+    }
+    protected boolean isSelected(By locator, int time) {
+        explicitWait(locator, time);
+        return webElement(locator).isSelected();
+    }
+    protected boolean isDisplayed(By locator, int time) {
+        explicitWait(locator, time);
+        return webElement(locator).isDisplayed();
+    }
+    protected boolean isEnabled(By locator, int time) {
+        explicitWait(locator, time);
+        return webElement(locator).isEnabled();
     }
 
 
